e0198d46dfce2a42b66b9cedcc2e98867f8fdc82
chrishi <chap.rishiqa@gmail.com>
2024-09-11 12:45:30 -0500

    updated scripts to generate allure reports

--- wdio.local.conf.ts
+++ wdio.local.conf.ts
@@ -1,8 +1,14 @@
 import { config as sharedConfig } from './wdio.shared.conf.js'
 
+
 export const config: WebdriverIO.Config = {
     ...sharedConfig,
     ...{
+        reporters: [['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+    }]],
         capabilities: [{
             browserName: 'chrome'
             // 'goog:chromeOptions': {
